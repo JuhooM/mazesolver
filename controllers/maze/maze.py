72568027ed05ad111a0d142038506e5875094b0f
JuhooM <juho.matilainen190@gmail.com>
2023-11-09 19:08:19 +0200

    Update
    Big update, all movement functions work, added movement correcting functions, better maze solving works with good wall following using movement functions.

--- controllers/maze/maze.py
+++ controllers/maze/maze.py
@@ -8,14 +8,13 @@ robot = Robot()
 timeStep = int(robot.getBasicTimeStep())
 
 # constants
-MAX_SPEED = 6.28 # maximum speed of robot
-SENSOR_THRESHOLD = 90 # sensitivity of sensors surrounding robot for obstacle detection
+MAX_SPEED = 4 # speed of robot
+SENSOR_THRESHOLD = 80 # sensitivity of sensors surrounding robot for obstacle detection
 UNIT_SIZE = 0.125 # size of 1 maze node in mmeters
-WHEEL_RADIUS = 0.0205 # radius of robots wheel in mmeters
-AXLE_LENGTH = 0.052 # lenght of the robots axle of wheels in mmeters
+WHEEL_RADIUS = 0.02002 # radius of robots wheel in mmeters
+AXLE_LENGTH = 0.0568 # lenght of the robots axle of wheels in mmeters
 
 # variables
-vel = [0.0, 0.0] # wheel speed [left wheel speed, right wheel speed]
 pos = [0.0, 0.0] # wheel position changed in rad [left wheel pos, right wheel pos]
 dis = [0.0, 0.0] # wheel distance traveled in meters [left wheel dis, right wheel dis]
 dirvec = [0.0, 0.0, 0.0] # position vector of robot [x axis position(robot face at start)-meter, y axis position-meter, orientation-rad]
@@ -25,8 +24,8 @@ left_motor = robot.getDevice('left wheel motor')
 right_motor = robot.getDevice('right wheel motor')
 left_motor.setPosition(float('inf'))
 right_motor.setPosition(float('inf'))
-left_motor.setVelocity(vel[0])
-right_motor.setVelocity(vel[1])
+left_motor.setVelocity(0)
+right_motor.setVelocity(0)
 
 # get a handler to the position sensors and enable them.
 left_position_sensor = robot.getDevice('left wheel sensor')
@@ -58,83 +57,93 @@ def disTravelled(turn: bool):
         
         # update dirvec
         if turn:
-            if distance1 < 0 or distance2 < 0:
-                if distance1 <= 0:
-                    dirvec[2] += (abs(distance1) + abs(distance2))/AXLE_LENGTH;
-                else:
-                    dirvec[2] -= (abs(distance1) + abs(distance2))/AXLE_LENGTH;
+            #if distance1 < 0 or distance2 < 0:
+            if distance1 < 0 and distance2 > 0:
+                dirvec[2] += (abs(distance1) + abs(distance2))/AXLE_LENGTH;
+            elif distance1 > 0 and distance2 < 0:
+                dirvec[2] -= (abs(distance1) + abs(distance2))/AXLE_LENGTH;
         else:
             dirvec[0] += avgdist * math.cos(dirvec[2])
             dirvec[1] += avgdist * math.sin(dirvec[2])
-                
-        
-        print('Robots position [x-axis, y-axis, orientation]')
-        print(dirvec)
-        print(' ')
         
         return avgdist
-    
-# sets robots speed to match vel values
-def setSpeed():
-    left_motor.setVelocity(vel[0])
-    right_motor.setVelocity(vel[1])
 
 # turn right turn_amount degrees
 def turn_right(turn_amount_deg):
-    stop()
+    goal = dirvec[2] - turn_amount_deg/360 * 2 * math.pi
     # set left wheel speed
-    vel[0] = MAX_SPEED/10
+    left_motor.setVelocity(MAX_SPEED*0.33)
     # set right wheel speed
-    vel[1] = -MAX_SPEED/10
-    setSpeed()
+    right_motor.setVelocity(-MAX_SPEED*0.33)
     
     # turn right wanted amount
-    goal = dirvec[2] - turn_amount_deg/360 * 2 * math.pi
-    while robot.step(timeStep) != -1:
+    while True:
         disTravelled(True)
-        if pow(goal-dirvec[2], 2) < 0.00004:
+        robot.step(timeStep)
+        if pow(goal-dirvec[2], 2) < 0.001:
             break
 
 # turn left turn_amount degrees
 def turn_left(turn_amount_deg):
-    stop()
+    goal = dirvec[2] + turn_amount_deg/360 * 2 * math.pi
     # set left wheel speed
-    vel[0] = -MAX_SPEED/10
+    left_motor.setVelocity(-MAX_SPEED*0.33)
     # set right wheel speed
-    vel[1] = MAX_SPEED/10
-    setSpeed()
+    right_motor.setVelocity(MAX_SPEED*0.33)
 
     # turn left wanted amount
-    goal = dirvec[2] + turn_amount_deg/360 * 2 * math.pi
-    while robot.step(timeStep) != -1:
+    while True:
         disTravelled(True)
-        if pow(goal-dirvec[2], 2) < 0.00004:
+        robot.step(timeStep)
+        if pow(goal-dirvec[2], 2) < 0.001:
             break
 
 # go forward distance in meters
 def forward(distance_m):
-    stop()
-    # set left wheel speed
-    vel[0] = MAX_SPEED
-    # set right wheel speed
-    vel[1] = MAX_SPEED
-    setSpeed()
-    
-    # go forward wanted distance
     goal = distance_m
     travelled = 0
-    while robot.step(timeStep) != -1:
-            travelled += disTravelled(False)
-            if pow(goal-travelled, 2) < 0.00001:
-                break
-            
+    # set left wheel speed
+    left_motor.setVelocity(MAX_SPEED)
+    # set right wheel speed
+    right_motor.setVelocity(MAX_SPEED)
+    # go forward wanted distance
+    while True:
+        travelled += disTravelled(False)
+        robot.step(timeStep)
+        
+        PID_correction()
+        
+        left = ps[7].getValue()
+        right = ps[0].getValue()
+        if pow(goal-travelled, 2) < 0.00008 or (left + right)/2 >= SENSOR_THRESHOLD:
+            break
+    stop()
+    delay(100)
+    
+    if (right + left) / 2 > SENSOR_THRESHOLD:
+        front_correct(left, right)
+        
+def bforward(distance_m):
+    revolutions = maze_parameters.TILE_LENGTH / robot_parameters.WHEEL #rev in radians
+    
+    left_wheel_revolutions = ps_left.getValue()
+    right_wheel_revolutions = ps_right.getValue()
+
+    left_wheel_revolutions += revolutions
+    right_wheel_revolutions += revolutions
+
+    left_motor.setVelocity(robot_parameters.SPEED)
+    right_motor.setVelocity(robot_parameters.SPEED)
+
+    left_motor.setPosition(left_wheel_revolutions)
+    right_motor.setPosition(right_wheel_revolutions)
+              
 # stop robot movement function
 def stop():
     #set left wheel speed
-    vel[0] = 0
+    left_motor.setVelocity(0)
     #set right wheel speed
-    vel[1] = 0
-    setSpeed()
+    right_motor.setVelocity(0)
 
 # delay time function for delay_ms
 def delay(delay_ms):
@@ -143,17 +152,124 @@ def delay(delay_ms):
         if (robot.getTime() - initTime) * 1000.0 > delay_ms:
             break
 
+# function to correct robot distance from left and right wall
+def PID_correction():
+    while True:
+        distance_left_now = left_position_sensor.getValue()
+        distance_right_now = right_position_sensor.getValue()
+
+        right_angle_sensor = ps[1].getValue()
+        left_angle_sensor = ps[6].getValue()
+        left_wall = ps[5].getValue()
+        right_wall = ps[2].getValue()
+        
+        left = ps[7].getValue()
+        right = ps[0].getValue()
+
+        previous_error = 0.00
+        error_integral = 0.00
+        P = 0.005  #0.005
+        I = 0.0008 #0.0005  0.0001
+        D = 0.0005 # 0.0002
+        Middle = 75
+        
+        if left_wall and right_wall:  
+            error = left_angle_sensor - right_angle_sensor
+            error_integral += error
+            error_derivative = (previous_error - error)
+            previous_error = error
+            MotorSpeed = P * error + I * error_integral + D * error_derivative
+            if MotorSpeed > 0.2:
+                MotorSpeed = 0.2
+            elif MotorSpeed < -0.2:
+                MotorSpeed = -0.2
+
+            left_motor.setVelocity(MAX_SPEED + MotorSpeed)
+            right_motor.setVelocity(MAX_SPEED - MotorSpeed)
+
+        elif left_wall:
+            error = left_angle_sensor - Middle           
+            error_integral += error
+            error_derivative = (previous_error - error)
+            previous_error = error
+            MotorSpeed = P * error + I * error_integral + D * error_derivative
+            if MotorSpeed > 0.06:
+                MotorSpeed = 0.06
+            elif MotorSpeed < -0.06:
+                MotorSpeed = -0.06
+
+            left_motor.setVelocity(MAX_SPEED + MotorSpeed)
+            right_motor.setVelocity(MAX_SPEED - MotorSpeed)
+
+        elif right_wall:
+            error = right_angle_sensor - Middle
+
+            error_integral += error
+            error_derivative = (previous_error - error)
+            previous_error = error
+            MotorSpeed = P * error + I * error_integral + D * error_derivative
+            if MotorSpeed > 0.06:
+                MotorSpeed = 0.06
+            elif MotorSpeed < -0.06:
+                MotorSpeed = -0.06
+
+            left_motor.setVelocity(MAX_SPEED - MotorSpeed)
+            right_motor.setVelocity(MAX_SPEED + MotorSpeed)
+    
+        distance_left_later = left_position_sensor.getValue()
+        distance_right_later = right_position_sensor.getValue()
+
+        if (distance_left_now == distance_left_later) and (distance_right_now == distance_right_later):
+            break
+
+# function correct robot distance from front wall 
+def front_correct(left, right):
+    left_motor.setPosition(float('inf'))
+    right_motor.setPosition(float('inf'))
+    
+    front = (left + right) / 2
+    desired_distance = SENSOR_THRESHOLD + 20
+
+    if left < right:
+        while left < right:
+            left_motor.setVelocity(MAX_SPEED * 0.1)
+            right_motor.setVelocity(MAX_SPEED * -0.1)
+            robot.step(timeStep)
+            left = ps[7].getValue()
+            right = ps[0].getValue()
+
+    elif left > right:
+        while left > right:
+            left_motor.setVelocity(MAX_SPEED * -0.1)
+            right_motor.setVelocity(MAX_SPEED * 0.1)
+            robot.step(timeStep)
+            left = ps[7].getValue()
+            right = ps[0].getValue()
+        
+    if front < desired_distance:
+        while front < desired_distance:
+            left_motor.setVelocity(MAX_SPEED * 0.1)
+            right_motor.setVelocity(MAX_SPEED * 0.1)
+            robot.step(timeStep)
+            left = ps[7].getValue()
+            right = ps[0].getValue()
+            front = (left + right) / 2
+
+    elif front > desired_distance:
+        while front > desired_distance:
+            left_motor.setVelocity(MAX_SPEED * -0.1)
+            right_motor.setVelocity(MAX_SPEED * -0.1)
+            robot.step(timeStep)
+            left = ps[7].getValue()
+            right = ps[0].getValue()
+            front = (left + right) / 2
+
 # better wall following maze solving
 def betterwallfollowSolve():
-    # get sensors values
-    sensors_value = []
-    for i in range(8):
-        sensors_value.append(ps[i].getValue())
-        
     # sensor data
-    left = sensors_value[5]
-    right = sensors_value[2]
-    front = (sensors_value[7]+sensors_value[0])/2
+    left = ps[5].getValue()
+    right = ps[2].getValue()
+    front = (ps[7].getValue() + ps[0].getValue())/2
     
     # location data in maze
     left_wall = left > SENSOR_THRESHOLD
@@ -169,18 +285,18 @@ def betterwallfollowSolve():
             forward(UNIT_SIZE)
         else:
             turn_right(90)
+    
+    #stop()
+    #delay(100)   
+    #if dirvec[0] >= 0.35 and dirvec[0] <= 0.625 and dirvec[1] >= 0.35 and dirvec[1] <= 0.625:
+        #return True
             
 # left wall following maze solving example, only works in perfect maze and gets stuck sometimes
-def wallfollowSolve():
-    # get sensors values
-    sensors_value = []
-    for i in range(8):
-        sensors_value.append(ps[i].getValue())
-        
+def wallfollowSolve():        
     # sensor data
-    left_wall = sensors_value[5] > SENSOR_THRESHOLD+20
-    left_corner = sensors_value[6] > SENSOR_THRESHOLD
-    front_wall = (sensors_value[7]+sensors_value[0])/2 > SENSOR_THRESHOLD+20
+    left_wall = ps[5].getValue() > SENSOR_THRESHOLD+20
+    left_corner = ps[6].getValue() > SENSOR_THRESHOLD
+    front_wall = (ps[7].getValue() + ps[0].getValue())/2 > SENSOR_THRESHOLD+40
     
     # intial speed
     left_speed = MAX_SPEED
@@ -202,11 +318,12 @@ def wallfollowSolve():
             right_speed = MAX_SPEED/6
 
     # Set motor vel
-    delay(10)
+    delay(50)
     left_motor.setVelocity(left_speed)
     right_motor.setVelocity(right_speed)
 
-# main loop for robot functions
+# main loop
 while robot.step(timeStep) != -1:
-    #wallfollowSolve()
-    betterwallfollowSolve()
+    #wallfollowSolve():
+    if betterwallfollowSolve():
+        break
